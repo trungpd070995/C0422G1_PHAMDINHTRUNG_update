4e6eca09dbfdafb18109299a9d36ecafe1504eba
unknown <you@example.com>
2022-07-19 08:25:49 +0700

    fix

--- _03_database_query/exercise/quan_ly_ban_hang/quan_ly_ban_hang.sql
+++ _03_database_query/exercise/quan_ly_ban_hang/quan_ly_ban_hang.sql
@@ -62,3 +62,46 @@ INSERT INTO order_detail (o_id, p_id, od_qty) VALUES
 (2 ,5, 4),
 (2 ,3, 3);
 
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+SELECT 
+    o_id, o_date, o_total_price
+FROM
+    `order`;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+SELECT 
+    customer.c_name, product.p_name
+FROM
+    customer
+        INNER JOIN
+    `order` ON customer.c_id = `order`.c_id
+        INNER JOIN
+    order_detail ON `order`.o_id = order_detail.o_id
+        INNER JOIN
+    product ON order_detail.p_id = product.p_id;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+SELECT 
+    customer.*, COUNT(`order`.c_id) AS san_pham
+FROM
+    customer
+        LEFT JOIN
+    `order` ON customer.c_id = `order`.c_id
+GROUP BY c_id
+HAVING COUNT(`order`.c_id) = 0;
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+-- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+SELECT 
+    `order`.o_id,
+    `order`.o_date,
+    SUM(order_detail.od_qty * product.p_price)
+FROM
+    `order`
+        JOIN
+    order_detail ON `order`.o_id = order_detail.o_id
+        JOIN
+    product ON order_detail.p_id = product.p_id
+GROUP BY o_id;
+
+
--- _03_database_query/exercise/quan_ly_sinh_vien/quan_ly_sinh_vien.sql
+++ _03_database_query/exercise/quan_ly_sinh_vien/quan_ly_sinh_vien.sql
@@ -65,4 +65,35 @@ VALUES
 (1, 1, 8, 1),
 (1, 2, 10, 2),
 (2, 1, 12, 1);
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+select * from student
+where student_name like "H%";
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+select * from class
+where month(start_day) = 12;
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5
+select * from `subject`
+where credit between 3 and 5;
+
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+SET SQL_SAFE_UPDATES = 0;
+UPDATE student 
+SET 
+    class_id = 2
+WHERE
+    `student_name` = 'Hung';
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+SELECT 
+    student.student_name, `subject`.sub_name, mark
+FROM
+    mark
+        INNER JOIN
+    student ON mark.student_id = student.student_id
+        INNER JOIN
+    `subject` ON mark.sub_id = `subject`.sub_id
+ORDER BY mark DESC , student.student_name;
        
